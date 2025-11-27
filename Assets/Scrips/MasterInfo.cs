04f3324d90badd4a1383f91db378b018e463c199
Yanca Vitória <yancav.gouveia@gmail.com>
2025-11-27 14:06:55 -0400

    gemas e contagem (sem mostrar na tela)

--- Assets/Scrips/MasterInfo.cs
+++ Assets/Scrips/MasterInfo.cs
@@ -6,9 +6,14 @@ public class MasterInfo : MonoBehaviour
 {
     public static int coinCount = 0;
     [SerializeField] GameObject coinDisplay;
+    public static int gemCount = 0;
+    public static int distanceRun;
+    [SerializeField] int internalDistance;
+
 
     void Update()
     {
+        internalDistance = distanceRun;
         coinDisplay.GetComponent<TMPro.TMP_Text>().text = "COINS: " + coinCount;
     }
 }
